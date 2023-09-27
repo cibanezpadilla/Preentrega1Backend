c91a52d465d48ad0f6a1b46c177b8248482b19cf
Clemencia Ibanez Padilla <clemenciaibanezpadilla@gmail.com>
2023-09-27 20:03:35 -0300

    mas validaciones

--- src/cartManager.js
+++ src/cartManager.js
@@ -8,10 +8,7 @@ class CartManager {
         this.path = path
     }
 
-
-    async getCarts() {
-
-    }
+    
 
     async createCart() {
         try {
@@ -64,33 +61,40 @@ class CartManager {
 
 
     async addProductToCart(cid, pid){
-        if (existsSync(this.path)){
-            const cartProductsFile= await promises.readFile(this.path, 'utf-8')
-            
-            const cartProductsParseado= JSON.parse(cartProductsFile)
-            const selectedCart = cartProductsParseado.find(c=> c.id === cid)
-
-            if (selectedCart){
-                const product = await manager.getProductById(pid);
-                if (!product) {
-                  throw new Error("There is no product with this id");
-                }
-
-                const prodExists = selectedCart.products.find(p=>p.pid === pid)
-                if (prodExists) {
-                    const index = selectedCart.products.findIndex(p=> p.pid == pid)
-                    selectedCart.products[index].quantity += 1                    
-                }else{
-                    const newProd ={
-                        pid, 
-                        quantity: 1
-                    }
-                    selectedCart.products.push(newProd)
-                }
+        try{
+            if (existsSync(this.path)){
+                const cartProductsFile= await promises.readFile(this.path, 'utf-8')
                 
-                await promises.writeFile(this.path, JSON.stringify(cartProductsParseado))
-            }            
-            return selectedCart            
+                const cartProductsParseado= JSON.parse(cartProductsFile)
+                const selectedCart = cartProductsParseado.find(c=> c.id === cid)
+    
+                if (selectedCart){
+                    const product = await manager.getProductById(pid);
+                    if (!product) {
+                      throw new Error("There is no product with this id");
+                    }
+    
+                    const prodExists = selectedCart.products.find(p=>p.pid === pid)
+                    if (prodExists) {
+                        const index = selectedCart.products.findIndex(p=> p.pid == pid)
+                        selectedCart.products[index].quantity += 1                    
+                    }else{
+                        const newProd ={
+                            pid, 
+                            quantity: 1
+                        }
+                        selectedCart.products.push(newProd)
+                    }
+                    
+                    await promises.writeFile(this.path, JSON.stringify(cartProductsParseado))
+                }            
+                return selectedCart            
+            }else{
+                throw new Error("The file does not exist");
+            }
+        }
+        catch(error){
+            throw new Error(error.message);
         }  
         }    
 }
